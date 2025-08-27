cb5e9e978f873e5090ac8d97f795000b42a2aa41
Preeti_Bca <91714205+Preeti-Bca@users.noreply.github.com>
2025-08-27 15:38:46 +0530

    Update app.py to use DATABASE_URL from Render

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ import os
 import random
 import string
 import re
+from flask_migrate import Migrate
+
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'starblue-secret-key-2024'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 
 # Database Models
 class User(db.Model):
