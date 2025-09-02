e23e229b04d8224b1e60cc7f5da40f49bee083f3
Preeti_Bca <91714205+Preeti-Bca@users.noreply.github.com>
2025-09-02 10:33:08 +0530

    Update admin default password to kuldeep123

--- app.py
+++ app.py
@@ -258,18 +258,22 @@ def init_db():
     db.create_all()
     
     # Check if admin user already exists
-    if not User.query.filter_by(username='admin').first():
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
         admin = User(
             username='admin',
             email='admin@starblue.com',
-            password_hash=generate_password_hash('kuldeep123'),
+            password_hash=generate_password_hash('kuldeep123', method="pbkdf2:sha256"),
             full_name='System Administrator',
             first_name='System',
             last_name='Administrator',
             phone='+1234567890',
             is_admin=True
-        )
+            )
         db.session.add(admin)
+    else:
+        admin.password_hash = generate_password_hash('kuldeep123', method="pbkdf2:sha256")
+
     
     # Check if regular user already exists
     if not User.query.filter_by(username='user').first():
